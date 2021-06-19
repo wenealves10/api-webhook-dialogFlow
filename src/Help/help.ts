d11e02597045fd42974f2b40198f9335754155e7
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-06-19 19:08:04 -0300

    Deploy 💯

--- src/Help/help.ts
+++ src/Help/help.ts
@@ -51,33 +51,6 @@ class Help {
 					],
 				});
 			}
-		} else if (queryResult.intent.displayName === "Produto") {
-			switch (queryResult.parameters.produto) {
-				case "Landing page":
-					response.json({
-						fulfillmentMessages: [
-							{
-								text: {
-									text: [
-										`Opa legal amigo você quer criar uma ${queryResult.parameters.produto} né?`,
-										"Vamos conversa mais sobre isso no Whatsapp pode ser? meu número é (99) 9 8852-5464",
-									],
-								},
-							},
-						],
-					});
-					break;
-				default:
-					response.json({
-						fulfillmentMessages: [
-							{
-								text: {
-									text: [`OK ${queryResult.parameters.produto}`],
-								},
-							},
-						],
-					});
-			}
 		}
 	}
 }

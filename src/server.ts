4b7361860a7a00400c57c9759af513345a710a97
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-06-19 17:00:44 -0300

    Deploy 💯

--- src/server.ts
+++ src/server.ts
@@ -8,25 +8,28 @@ app.use(express.json());
 
 app.post("/dialogflow", (request, response) => {
 	const { queryResult } = request.body as IRequestDialog;
-	if (queryResult.intent.displayName === "Servico - yes") {
-		response.json({
-			fulfillmentMessages: [
-				{
-					card: {
-						title: "Meu Portfólio",
-						subtitle: "Crio vários sites e sistemas web",
-						imageUri: "https://i.imgur.com/rKyEO9A.png",
-						buttons: [
-							{
-								text: "Quero ver o site",
-								postback: "https://www.wenedev.site",
-							},
-						],
-					},
-				},
-			],
-		});
+	if (queryResult.intent.displayName === "Ajuda") {
+		console.log(queryResult);
+		response.json({ fulfillmentMessages: "Ok" });
 	}
 });
 
 app.listen(process.env.PORT || 3000);
+
+// response.json({
+//   fulfillmentMessages: [
+//     {
+//       card: {
+//         title: "Meu Portfólio",
+//         subtitle: "Crio vários sites e sistemas web",
+//         imageUri: "https://i.imgur.com/rKyEO9A.png",
+//         buttons: [
+//           {
+//             text: "Quero ver o site",
+//             postback: "https://www.wenedev.site",
+//           },
+//         ],
+//       },
+//     },
+//   ],
+// });

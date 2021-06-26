e62a2faef7f8b0352de6749901878b441d4653cd
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-06-26 09:34:59 -0300

    Help 💯

--- src/Help/help.ts
+++ src/Help/help.ts
@@ -1,12 +1,13 @@
 import { Request, Response } from "express";
 import { IRequestDialog } from "../DialogFlowDto/resquest";
+import { SendMailer } from "../services/sendMailer";
 
 class Help {
 	async handle(request: Request, response: Response) {
 		const {
 			queryResult: {
 				intent: { displayName },
-				parameters: { servico },
+				parameters: { servico, email, message, name },
 			},
 		} = request.body as IRequestDialog;
 
@@ -35,9 +36,18 @@ class Help {
 				dialogflowResponse.followupEventInput.name = "product";
 				dialogflowResponse.followupEventInput.parameters.servico = "Aplicativo";
 			}
+			return response.json(dialogflowResponse);
 		}
 
-		return response.json(dialogflowResponse);
+		if (displayName === "product") {
+			const emails = new SendMailer();
+			emails.send(name, email, message, servico);
+			return response.json({
+				fulfillmentText: `${name} obrigado! vamos entrar em contato pelo e-mail`,
+			});
+		}
+
+		return response.json({});
 	}
 }
 
